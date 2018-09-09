6a7143a03de48ffbd1a728a53c14485179ee6acc
Priyajit Patra <priyajitpatra8@gmail.com>
2018-09-09 20:20:35 +0530

    Update ANOLOG_PWM_LED_CONTROL.ino

--- ANOLOG_PWM_LED_CONTROL.ino
+++ ANOLOG_PWM_LED_CONTROL.ino
@@ -1,24 +1,4 @@
-// Adafruit IO Analog Out Example
-// Tutorial Link: https://learn.adafruit.com/adafruit-io-basics-analog-output
-//
-// Adafruit invests time and resources providing this open source code.
-// Please support Adafruit and open source hardware by purchasing
-// products from Adafruit!
-//
-// Written by Todd Treece for Adafruit Industries
-// Copyright (c) 2016 Adafruit Industries
-// Licensed under the MIT license.
-//
-// All text above must be included in any redistribution.
-
-/************************** Configuration ***********************************/
-
-// edit the config.h tab and enter your Adafruit IO credentials
-// and any additional configuration needed for WiFi, cellular,
-// or ethernet clients.
 #include "config.h"
-
-/************************ Example Starts Here *******************************/
 
 // this should correspond to a pin with PWM capability
 #define LED_PIN 5
